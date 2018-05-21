057520a6c8953fb1f53033ef8e9434105b3909b9
Jεan Sacren <sakiwit@gmail.com>
2018-05-21 02:20:40 -0600

    Add destroy of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,12 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @article.comments.find(params[:id]).destroy
+    redirect_to article_path(@article)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
