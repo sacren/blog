a775e7c37555eeaa474b1c83d6d6c59fd4f00ac5
Jεan Sacren <sakiwit@gmail.com>
2018-05-22 01:03:53 -0600

    Add authentication for articles and comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'foo',
+                               password: 'bar',
+                               except: [:index, :show]
+
   def index
     @articles = Article.all
   end
